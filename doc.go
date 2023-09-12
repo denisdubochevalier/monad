96005b814c2d4ac51845dc18161f6c4b6eb3f29b
Denis Dubo Chevalier <denisdubochevalier@gmail.com>
2023-09-12 17:54:56 +0200

    Update package doc

--- doc.go
+++ doc.go
@@ -1,37 +1,47 @@
-// Package monad implements three commonly used monads: Maybe, Either, and
-// Result.
+// Package monad provides an extensive implementation of various monadic
+// structures, encapsulating a wide range of computational design patterns.
+// These include: Maybe, Either, Result, Identity, List, Reader, Writer, and
+// State.
 //
-// Monads are a category-theoretic concept that have found extensive
-// applicability in functional programming. They serve as "programmable
-// semicolons"; they dictate the order and side effects of function calls. While
-// Go is not purely functional, the design patterns provided by monads can be
-// useful for managing side-effectsand control flow.
+// Originating from category theory and serving as computational building blocks
+// in functional programming, monads enable fine-grained control over
+// programmatic side-effects, sequencing, and value context.
 //
-// The monads defined in this package adhere strictly to the three monadic laws:
+// Every monad implemented in this package rigorously adheres to the three
+// foundational monadic laws:
 // - Left Identity:  return a >>= f  is equivalent to  f(a)
 // - Right Identity: m >>= return  is equivalent to  m
 // - Associativity:  (m >>= f) >>= g  is equivalent to  m >>= (\x -> f x >>= g)
 //
-// Maybe Monad:
-// The Maybe monad is used for computations that may fail to return a value.
-// The Maybe monad encapsulates an optional value, and allows you to compose
-// functions that might not return a meaningful result.
+// The following monads are implemented:
+//   - Maybe: Represents optional values and handles the absence of a value.
+//   - Either: Extends Maybe by encapsulating an error or alternate reason for
+//     the absence of a value.
+//   - Result: Tailored to Go's idiomatic error handling, encapsulating either a
+//     successful result or an error.
+//   - Identity: The simplest monad, acting as a container for a single value.
+//   - List: Represents a collection of values in a monadic context.
+//   - Reader: Encapsulates a shared environment required by various
+//     computations.
+//   - Writer: Captures additional output during the computation, useful for
+//     logging or state tracing.
+//   - State: Allows the embedding of stateful computations within the monadic
+//     structure.
 //
-// Either Monad:
-// The Either monad extends Maybe to capture the reason for failure, often
-// utilized for error handling. Either has two subclasses, Left and Right; by
-// convention, Right is the "happy path" and Left captures an error or alternate
-// value.
+// Upcoming Monads:
+//   - Continuation Monad: Suited for asynchronous or nested computations.
+//   - IO Monad: To encapsulate effectful computations in a functional style.
+//   - Validation Monad: To accumulate all errors rather than failing fast,
+//     useful in form validation.
+//   - Future/Promise Monad: An academic endeavor given Go's native concurrency
+//     model.
+//   - Free Monad: An advanced construct to build interpreters for embedded
+//     DSLs.
 //
-// Result Monad:
-// Result is tailored to Go's idiomatic error handling. It represents the
-// outcome of a function that could fail, wrapping either a successful result or
-// an error. Unlike Maybe and Either, Result explicitly deals with Go's native
-// error type.
-//
-// Each monad has a set of functional methods, primarily `FlatMap` and `Or`, to
-// facilitate function composition, control flow, and side-effect management.
+// Each monad is endowed with functional methods like `FlatMap` and `Map` to
+// facilitate composability and side-effect management.
 //
 // Usage:
-// Refer to individual monad sub-packages for example usage and further details.
+// Consult the associated documentation for each individual monad to explore
+// example usage and further details.
 package monad
